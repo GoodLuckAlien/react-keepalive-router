1d684e448aa6a06b524636f965ea139ce209177e
zhaolinlin10 <zhaolinlin10@jd.com>
2022-03-29 17:13:16 +0800

    提交

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 
-import KeepaliveRouterSwitch from './components/keepliveRouteSwitch'
-import KeepaliveRoute from './components/keepliveRoute'
-import {GetCacheContext, useCacheDispatch} from './components/keepCache'
+// import KeepaliveRouterSwitch from './components/keepliveRouteSwitch'
+// import KeepaliveRoute from './components/keepliveRoute'
+// import {GetCacheContext, useCacheDispatch} from './components/keepCache'
 
-import cacheRouterContext from './core/cacheContext'
-import {addKeeperListener} from './core/keeper'
+// import cacheRouterContext from './core/cacheContext'
+// import {addKeeperListener} from './core/keeper'
 
-import keepaliveLifeCycle from './hoc/lifecycle'
+// import keepaliveLifeCycle from './hoc/lifecycle'
 
-export {KeepaliveRoute, KeepaliveRouterSwitch, cacheRouterContext, GetCacheContext, useCacheDispatch, addKeeperListener,keepaliveLifeCycle}
+// export {KeepaliveRoute, KeepaliveRouterSwitch, cacheRouterContext, GetCacheContext, useCacheDispatch, addKeeperListener,keepaliveLifeCycle}
+
+
